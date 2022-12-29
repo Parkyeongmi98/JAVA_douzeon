65c3de1309bf2d8690df292e70a4e20a6e027542
Parkyeongmi98 <yeongmi_@naver.com>
2022-12-29 09:52:23 +0900

    EchoServer #1

--- finaltest/src/main/java/prob03/Money.java
+++ finaltest/src/main/java/prob03/Money.java
@@ -4,4 +4,38 @@ public class Money {
 	private int amount;
 	
 	/* 코드 작성 */
+	public Money(int amount) {
+		this.amount = amount;
+	}
+
+	public Money add(Money money) {
+		int result = this.amount + money.amount;
+		Money m = new Money(result);
+		return m;
+	}
+	
+	public Money minus(Money money) {
+		int result = this.amount - money.amount;
+		Money m = new Money(result);
+		return m;
+	}
+	
+	public Money multiply(Money money) {
+		int result = this.amount * money.amount;
+		Money m = new Money(result);
+		return m;
+	}
+	
+	public Money devide(Money money) {
+		int result = this.amount / money.amount;
+		Money m = new Money(result);
+		return m;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof Money && this.amount == ((Money)obj).amount) {
+			return true;	
+		}
+		return false;
+	}
 }
--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,13 +2,27 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 	public static void main(String[] args) {
-		try {
-			InetAddress.getAllByName("www.douzone.com");
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+		while (true) {
+			try {
+				Scanner sc = new Scanner(System.in);
+				System.out.print(">>");
+				String str = sc.nextLine();
+				
+				if(str.equals("exit")) {
+					System.out.println("종료");
+					break;
+				}
+				
+				InetAddress inet = InetAddress.getByName(str);
+				System.out.println(inet.getHostName() + ":" + inet.getHostAddress());
+				
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
