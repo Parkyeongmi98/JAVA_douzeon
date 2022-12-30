ec55a1c90df5bf601837ba1bf93cb7b4d6fe4084
Parkyeongmi98 <yeongmi_@naver.com>
2022-12-30 21:04:07 +0900

    chat -ing

--- network/src/main/java/chat/ChatServer.java
+++ network/src/main/java/chat/ChatServer.java
@@ -1,9 +1,47 @@
 package chat;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatServer {
 	public static final int PORT = 9999;
+	private static List<Writer> listWriters = new ArrayList<>();
 	
 	public static void main(String[] args) {
-		
+		ServerSocket serverSocket = null;
+
+		try {
+			serverSocket = new ServerSocket();
+			
+			
+			String hostAddress = InetAddress.getLocalHost().getHostAddress();
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT), 10);
+			log("연결 기다림 " + hostAddress + ":" + PORT);
+			
+			while(true) {
+				Socket socket = serverSocket.accept();
+				new ChatServerThread(socket, listWriters).start();
+			}
+		} catch (IOException e) {
+			log("error: " + e); // 서버에서 나는 오류
+		} finally {
+			try {
+				if (serverSocket != null && !serverSocket.isClosed()) {
+					serverSocket.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static void log(String message) {
+		System.out.println("[ChatServer#" + Thread.currentThread().getId() + "]" + message);
 	}
 }
