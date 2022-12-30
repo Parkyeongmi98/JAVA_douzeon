a2b28df60795e87f49ff2f0959ad1eba4ce60a85
Parkyeongmi98 <yeongmi_@naver.com>
2022-12-30 13:55:36 +0900

    Socket Options #finish

--- network/src/main/java/test/TCPClient.java
+++ network/src/main/java/test/TCPClient.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class TCPClient {
 	private static final String SERVER_IP = "127.0.0.1";
@@ -16,6 +17,27 @@ public class TCPClient {
 		try {
 			// 1. 소켓 생성
 			socket = new Socket();
+			
+			// 1-1. 소켓 버퍼 사이즈 확인
+			int rcvBufferSize = socket.getReceiveBufferSize();
+			int sndBufferSize = socket.getSendBufferSize();
+			
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);
+			
+			// 1-2. 소켓 버퍼 사이즈 변경
+			socket.setReceiveBufferSize(1024 * 10);
+			socket.setSendBufferSize(1024 * 10);
+			
+			rcvBufferSize = socket.getReceiveBufferSize();
+			sndBufferSize = socket.getSendBufferSize();
+			
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);
+			
+			// 1-3. SO_NODELAY(Nagle Algorithm off) -> 원래는 켜져있음
+			socket.setTcpNoDelay(true); // false가 기본값(on)
+			
+			// 1-4. SO_TIMEOUT
+			socket.setSoTimeout(3000); // 3초 타임아웃
 			
 			// 2. 서버 연결		
 			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
@@ -40,8 +62,10 @@ public class TCPClient {
 			data = new String(buffer, 0, readByteCount, "utf-8");
 			System.out.println("[client] received: " + data);
 			
+		} catch(SocketTimeoutException ex) {
+			System.out.println("[client] time out!!!!");
 		} catch(SocketException ex) {
-			System.out.println("[server] suddenly closed by server");
+			System.out.println("[client] suddenly closed by server");
 		} catch (IOException e) {
 			System.out.println("[client] error: " + e);
 		} finally {
